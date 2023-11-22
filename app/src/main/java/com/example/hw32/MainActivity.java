5d06158fa1c57ebf9aed984b07823f9f1671d1ae
musabekov-1 <bestmusabekov@gmail.com>
2023-11-22 18:39:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw32/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hw32;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, new FoodFragment()).commit();
+    }
+}
